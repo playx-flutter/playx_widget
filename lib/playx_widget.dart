5f0de1b5d652073dcafeb06acb751a6920207117
Basem Osama <engbasemosama@gmail.com>
2024-02-20 10:17:26 +0200

    update to v0.1.2
    Signed-off-by: Basem Osama <engbasemosama@gmail.com>

--- lib/playx_widget.dart
+++ lib/playx_widget.dart
@@ -1,29 +1,29 @@
 library playx_widget;
 
 export 'package:cached_network_image/cached_network_image.dart';
+export 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:flutter_svg/svg.dart';
-export 'package:lottie/lottie.dart';
+export 'package:lottie/lottie.dart' hide Marker;
 export 'package:package_info_plus/package_info_plus.dart';
+
+export 'src/alerts/confirmation_dialog.dart';
+export 'src/widgets/anim_search_bar.dart';
+export 'src/widgets/animated_visibility.dart';
 export 'src/widgets/app_version.dart';
 export 'src/widgets/auto_animated_list.dart';
 export 'src/widgets/center_loading.dart';
+export 'src/widgets/dashed_line.dart';
+export 'src/widgets/dashed_vertical_line.dart' hide DashedLinePainter;
+export 'src/widgets/fade_indexed_stack.dart';
+export 'src/widgets/favorite_button.dart';
 export 'src/widgets/icon_viewer.dart';
 export 'src/widgets/image_viewer.dart';
 export 'src/widgets/keep_alive_wrapper.dart';
-export 'src/widgets/optimized_card.dart';
-export 'src/widgets/optimized_scroll_view.dart';
-export 'src/widgets/animated_visibility.dart';
-export 'src/widgets/optimized_text_field.dart';
-export 'src/widgets/timer_refresh_widget.dart';
-export 'src/alerts/confirmation_dialog.dart';
-export 'package:flutter_screenutil/flutter_screenutil.dart';
-export 'src/widgets/optimized_list_tile.dart';
-export 'src/widgets/read_more_text.dart';
-export 'src/widgets/anim_search_bar.dart';
 export 'src/widgets/marquee_widget.dart';
-export 'src/widgets/dashed_line.dart';
-export 'src/widgets/favorite_button.dart';
-export 'src/widgets/fade_indexed_stack.dart';
-export 'src/widgets/dashed_vertical_line.dart' hide DashedLinePainter;
-
-export 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+export 'src/widgets/optimized_card.dart';
+export 'src/widgets/optimized_list_tile.dart';
+export 'src/widgets/optimized_scroll_view.dart';
+export 'src/widgets/optimized_text_field.dart';
+export 'src/widgets/read_more_text.dart';
+export 'src/widgets/timer_refresh_widget.dart';
