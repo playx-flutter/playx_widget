96a4132eaf7ad46c79c4d37d8e740f265935adf5
Basem Osama <engbasemosama@gmail.com>
2025-01-09 14:14:58 +0200

    refactor:  Moved the following methods under the new `CommonImageUtils` class:         - `getImageFromSvgAsset`         - `getImageFromWidget` - CU-86c1jq0m0
    Signed-off-by: Basem Osama <engbasemosama@gmail.com>

--- lib/src/utils/common_utils.dart
+++ lib/src/utils/common_utils.dart
@@ -8,118 +8,120 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-Future<Uint8List> getImageFromSvgAsset(String assetName,
-    {Size size = const Size(48, 48),
+class CommonImageUtils {
+  static Future<Uint8List> getImageFromSvgAsset(String assetName,
+      {Size size = const Size(48, 48),
+      BuildContext? context,
+      ColorMapper? colorMapper}) async {
+    final pictureInfo = await vg.loadPicture(
+      SvgAssetLoader(
+        assetName,
+        colorMapper: colorMapper,
+      ),
+      null,
+    );
+
+    // ignore: use_build_context_synchronously
+    final view = _getFlutterView(context: context);
+    if (view == null) throw Exception('Could not find flutter view');
+
+    final devicePixelRatio = view.devicePixelRatio;
+    final width = (size.width * devicePixelRatio).toInt();
+    final height = (size.height * devicePixelRatio).toInt();
+
+    final scaleFactor = min(
+      width / pictureInfo.size.width,
+      height / pictureInfo.size.height,
+    );
+
+    final recorder = ui.PictureRecorder();
+
+    Canvas(recorder)
+      ..scale(scaleFactor)
+      ..drawPicture(pictureInfo.picture);
+
+    final rasterPicture = recorder.endRecording();
+
+    final image = rasterPicture.toImageSync(width, height);
+    final bytes = (await image.toByteData(format: ui.ImageByteFormat.png))!;
+
+    return bytes.buffer.asUint8List();
+  }
+
+  /// Creates an image from the given [widget] by
+  /// 1. Spinning up an element and render tree;
+  /// 2. Waiting for the given [delay];
+  /// 3. Creating an image via a [RepaintBoundary].
+  /// https://github.com/flutter/flutter/issues/40064
+  static Future<Uint8List> getImageFromWidget(
+    Widget widget, {
+    Duration delay = const Duration(milliseconds: 100),
     BuildContext? context,
-    ColorMapper? colorMapper}) async {
-  final pictureInfo = await vg.loadPicture(
-    SvgAssetLoader(
-      assetName,
-      colorMapper: colorMapper,
-    ),
-    null,
-  );
+    Size logicalSize = const Size(800, 600),
+  }) async {
+    final view = _getFlutterView(context: context);
+    if (view == null) throw Exception('Could not find flutter view');
 
-  // ignore: use_build_context_synchronously
-  final view = _getFlutterView(context: context);
-  if (view == null) throw Exception('Could not find flutter view');
+    final devicePixelRatio = view.devicePixelRatio;
+    final repaintBoundary = RenderRepaintBoundary();
 
-  final devicePixelRatio = view.devicePixelRatio;
-  final width = (size.width * devicePixelRatio).toInt();
-  final height = (size.height * devicePixelRatio).toInt();
+    final renderView = RenderView(
+      view: view,
+      child: RenderPositionedBox(
+          alignment: Alignment.center, child: repaintBoundary),
+      configuration: ViewConfiguration(
+        physicalConstraints:
+            BoxConstraints.tight(logicalSize) * view.devicePixelRatio,
+        logicalConstraints: BoxConstraints.tight(logicalSize),
+        devicePixelRatio: view.devicePixelRatio,
+      ),
+    );
 
-  final scaleFactor = min(
-    width / pictureInfo.size.width,
-    height / pictureInfo.size.height,
-  );
+    final pipelineOwner = PipelineOwner()..rootNode = renderView;
+    renderView.prepareInitialFrame();
 
-  final recorder = ui.PictureRecorder();
+    final buildOwner = BuildOwner(focusManager: FocusManager());
 
-  Canvas(recorder)
-    ..scale(scaleFactor)
-    ..drawPicture(pictureInfo.picture);
+    final rootElement = RenderObjectToWidgetAdapter(
+      container: repaintBoundary,
+      child: Directionality(
+        textDirection: ui.TextDirection.ltr,
+        child: IntrinsicHeight(child: IntrinsicWidth(child: widget)),
+      ),
+    ).attachToRenderTree(buildOwner);
 
-  final rasterPicture = recorder.endRecording();
+    buildOwner.buildScope(rootElement);
 
-  final image = rasterPicture.toImageSync(width, height);
-  final bytes = (await image.toByteData(format: ui.ImageByteFormat.png))!;
+    if (delay > Duration.zero) await Future.delayed(delay);
 
-  return bytes.buffer.asUint8List();
-}
+    buildOwner
+      ..buildScope(rootElement)
+      ..finalizeTree();
+    pipelineOwner
+      ..flushLayout()
+      ..flushCompositingBits()
+      ..flushPaint();
 
-/// Creates an image from the given [widget] by
-/// 1. Spinning up an element and render tree;
-/// 2. Waiting for the given [delay];
-/// 3. Creating an image via a [RepaintBoundary].
-// https://github.com/flutter/flutter/issues/40064
-Future<Uint8List> getImageFromWidget(
-  Widget widget, {
-  Duration delay = const Duration(milliseconds: 100),
-  BuildContext? context,
-  Size logicalSize = const Size(800, 600),
-}) async {
-  final view = _getFlutterView(context: context);
-  if (view == null) throw Exception('Could not find flutter view');
+    final image = await repaintBoundary.toImage(pixelRatio: devicePixelRatio);
 
-  final devicePixelRatio = view.devicePixelRatio;
-  final repaintBoundary = RenderRepaintBoundary();
-
-  final renderView = RenderView(
-    view: view,
-    child: RenderPositionedBox(
-        alignment: Alignment.center, child: repaintBoundary),
-    configuration: ViewConfiguration(
-      physicalConstraints:
-          BoxConstraints.tight(logicalSize) * view.devicePixelRatio,
-      logicalConstraints: BoxConstraints.tight(logicalSize),
-      devicePixelRatio: view.devicePixelRatio,
-    ),
-  );
-
-  final pipelineOwner = PipelineOwner()..rootNode = renderView;
-  renderView.prepareInitialFrame();
-
-  final buildOwner = BuildOwner(focusManager: FocusManager());
-
-  final rootElement = RenderObjectToWidgetAdapter(
-    container: repaintBoundary,
-    child: Directionality(
-      textDirection: ui.TextDirection.ltr,
-      child: IntrinsicHeight(child: IntrinsicWidth(child: widget)),
-    ),
-  ).attachToRenderTree(buildOwner);
-
-  buildOwner.buildScope(rootElement);
-
-  if (delay > Duration.zero) await Future.delayed(delay);
-
-  buildOwner
-    ..buildScope(rootElement)
-    ..finalizeTree();
-  pipelineOwner
-    ..flushLayout()
-    ..flushCompositingBits()
-    ..flushPaint();
-
-  final image = await repaintBoundary.toImage(pixelRatio: devicePixelRatio);
-
-  final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-  return byteData!.buffer.asUint8List();
-}
-
-ui.FlutterView? _getFlutterView({BuildContext? context}) {
-  if (context != null) {
-    return View.of(context);
+    final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    return byteData!.buffer.asUint8List();
   }
 
-  final views = WidgetsBinding.instance.platformDispatcher.views;
-  if (views.isNotEmpty) {
-    return WidgetsBinding.instance.platformDispatcher.views.first;
-  }
-  return null;
-}
+  static ui.FlutterView? _getFlutterView({BuildContext? context}) {
+    if (context != null) {
+      return View.of(context);
+    }
 
-bool get isCupertino {
-  if (kIsWeb) return false;
-  return Platform.isIOS || Platform.isMacOS;
+    final views = WidgetsBinding.instance.platformDispatcher.views;
+    if (views.isNotEmpty) {
+      return WidgetsBinding.instance.platformDispatcher.views.first;
+    }
+    return null;
+  }
+
+  static bool get isCupertino {
+    if (kIsWeb) return false;
+    return Platform.isIOS || Platform.isMacOS;
+  }
 }
